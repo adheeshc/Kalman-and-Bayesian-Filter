d5e2792e5f61a1cf3de9cab7a3cfba6da419c16e
Adheesh <adheeshchat@gmail.com>
2019-11-14 02:42:17 -0500

    added effect of acceleration

--- gH_filter/tester.py
+++ gH_filter/tester.py
@@ -5,12 +5,37 @@ import matplotlib.pyplot as plt
 def genData(x0, dx, count, noise_factor):
 	return [x0 + dx*i + np.random.randn()*noise_factor for i in range(count)]
 
+def accData(x0, dx, count, noise_factor,accel=0):
+	zs=[]
+	for i in range(count):
+		zs.append(x0 + dx*i + np.random.randn()*noise_factor)
+		dx+=accel
+	return zs
+
+
+
 def main():
-	x0=160.0
-	dx=1
-	g=6/10
+	
+	x0=160.0 #Init estimate
+	dx=1 #change rate
+	
+	g=6/10 
+	
+	"""
+		setting g~1, signal exactly follows the measurements implies NO NOISE REJECTION
+		setting g~0, signal ignore measurements and follows prediction, implies noise filterd but also LEGITIMATE CHANGE IN SIGNAL
+	
+	"""
+
 	h=2/3
-	dt=1	
+	
+	"""
+		setting h~1, signal reacts to change rate RAPIDLY, signal amplitude is less and settles on measurements fast
+		setting h~0, signal reacts to change less SLOWLY, signal amplitude is large and settles on measurements slowly
+	
+	"""
+
+	dt=1	#time step
 
 	#LESS NOISE
 	measurements=genData(x0,dx,count=30,noise_factor=1)
@@ -28,6 +53,8 @@ def main():
 	#gh_extreme.toString()
 
 	#EFFECT OF ACCELERATION
+	accel_data = accData(x0=10., dx=0., count=20, noise_factor=0, accel=2.)
+	gh_accel=gH_filter(accel_data,x0,dx,g,h,dt,plot=True)
 
 if __name__=="__main__":
 	main()
