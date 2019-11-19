433597e7512d6434b8fd0b728ce37a91325fef93
Adheesh <adheeshchat@gmail.com>
2019-11-19 18:42:48 -0500

    example file modified to use filter class

--- Discrete Bayesian Filter/example.py
+++ Discrete Bayesian Filter/example.py
@@ -41,76 +41,19 @@ class Train(object):
 				pos -= 1
 		return pos
 
-
-def predict(pdf,offset,kernel):
-	return convolve(np.roll(pdf, offset), kernel, mode='wrap')
-	
-def update(likelihood, prior):
-	posterior = prior * likelihood
-	return normalize(posterior)
-
-def normalize(pdf):
-	pdf /= sum(np.asarray(pdf, dtype=float))
-	return pdf
-
-def lhScale(hall, z, z_prob):
-	try:
-		scale = z_prob / (1. - z_prob)
-	except ZeroDivisionError:
-		scale = 1e8
-	likelihood = np.ones(len(hall))
-	likelihood[hall==z] *= scale
-	return likelihood
-
-def barPlot(pos,x=None,ylim=(0,1),c='#30a2da',title=None):
-	ax = plt.gca()
-	if x is None:
-		x = np.arange(len(pos))
-	ax.bar(x,pos,color=c)
-	if ylim:
-		plt.ylim(ylim)
-	plt.xticks(np.asarray(x),x)
-	if title is not None:
-		plt.title(title)	
-	plt.show()
-
-
-def train_filter(iterations, kernel, sensor_accuracy, move_distance, do_print=True):
-	track = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+def train_filter(iterations, kernel, sensor_accuracy, offset, do_print=True):
+	scale = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 	prior = np.array([.9] + [0.01]*9)
 	posterior = prior[:]
-	normalize(prior)
+	measurements=[]
 
-	robot = Train(len(track), kernel, sensor_accuracy)
+	robot = Train(len(scale), kernel, sensor_accuracy)
 	for i in range(iterations):
-		# move the robot and
-		robot.move(distance=move_distance)
-
-
-
-		# peform prediction
-		prior = predict(posterior, move_distance, kernel)       
-
-		#  and update the filter
-		m = robot.sense()
-		likelihood = lhScale(track, m, sensor_accuracy)
-		posterior = update(likelihood, prior)
-		index = np.argmax(posterior)
-
-		if do_print:
-			print(f'time {i}: pos {robot.pos}, sensed {m}, at position {track[robot.pos]}')
-
-			print(f'estimated position is {index} with confidence {(posterior[index]*100):.4f}%:')
-			
-
-	barPlot(posterior)
-
-	if do_print:
-		print()
-		print('final position is', robot.pos)
-		index = np.argmax(posterior)
-		print(f'Estimated position is {index} with confidence {(posterior[index]*100):.4f}%:')
+		measurements.append(robot.move(distance=offset))
+	
+	dbf=Discrete_Bayes_Filter(prior,kernel,measurements,offset,sensor_accuracy,scale,doPrint=True)
+	for i in range(iterations):
+		dbf.plotPosterior(i)
 
 if __name__=="__main__":
-	train_filter(4, kernel=[.1, .8, .1], sensor_accuracy=.9,
-         move_distance=4, do_print=True)
+	train_filter(4,kernel=[.1, .8, .1], sensor_accuracy=.9,offset=4, do_print=True)
