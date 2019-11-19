88c8aadc3be13629e5c06c7f377fe1ddf088f047
Adheesh <adheeshchat@gmail.com>
2019-11-19 18:42:08 -0500

    final commit

--- Discrete Bayesian Filter/tester.py
+++ Discrete Bayesian Filter/tester.py
@@ -3,21 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-	prior = np.array([.1] * 10)
+	prior = np.array([.9] + [0.01] * 9)
 	kernel=(.1,.8,.1)
-	z_prob=1.0
-	scale=np.array([1,1,0,0,0,0,0,0,1,0])
-	offset=1
-	
-	# measurements with no noise
-	#measurements = [scale[i % len(scale)] for i in range(50)]
-	
+	z_prob=0.9
+	offset=4
+	scale=np.array([0,1,2,3,4,5,6,7,8,9])
+	measurements = [4,8,3,7]
+	doPrint=True
 
-	measurements = [1, 0, 1, 0, 0, 1, 1]
 
-	dbf = Discrete_Bayes_Filter(prior, kernel, measurements,offset, z_prob,scale)
-	dbf.plotPosterior(6)
-	dbf.plotPriors(6)
+	dbf = Discrete_Bayes_Filter(prior, kernel, measurements,offset, z_prob,scale,doPrint)
+	for i in range(len(measurements)):
+		dbf.plotPosterior(i)
 	
 
 if __name__=="__main__":
