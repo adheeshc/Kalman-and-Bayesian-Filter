61edc1296ca0f285a7e0a13b477e1ec3716cff7c
Adheesh <adheeshchat@gmail.com>
2019-11-22 20:17:25 -0500

    Final commit - bugs fixed

---
+++ gH Filter/gh_filter.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class gH_filter:
+	"""
+	Performs g-h filter on 1 state variable with a fixed g and h value
+
+	'data' contains the data to be filtered
+	'x0' is the intial value of the state variable
+	'dx' is the initial change rate of the state variable
+	'g' is the g scale factor
+	'h' is the h scale factor
+	'dt' is the time step
+	'plot' boolean variable that turns plotting results on/off
+	"""
+
+	def __init__(self,data,x0,dx,g,h,dt,plot=False):
+		self.data=data
+		self.x0=x0
+		self.dx=dx
+		self.g=g
+		self.h=h
+		self.dt=dt
+		self.results=self.filter()
+		if plot:
+			self.plotResults()
+
+	def filter(self):
+		results=[]
+		x_est=self.x0
+		for z in self.data:
+			#prediction
+			x_pred=x_est+(self.dx*self.dt)
+			self.dx=self.dx
+
+			#update
+			residual = z - x_pred
+			self.dx+=self.h*(residual)/self.dt
+			x_est=x_pred+self.g*residual
+			results.append(x_est)
+		return np.array(results)
+
+	def toString(self):
+		print(f'final estimates are \n{np.array(self.results)}')
+
+
+	def plotResults(self):
+		if type(self.data)==list:
+			size=len(self.data)
+		elif type(self.data)==np.ndarray:
+			size=self.data.shape[0]
+		x=np.linspace(1,size,size)
+		y=np.linspace(self.x0,self.results[-1],size)
+		plt.scatter(x,self.data,label='Measurements')
+		plt.plot(x,self.results,'r-',label='Estimate')
+		#plt.plot(x,y,'k--',label='Actual')
+		plt.grid()
+		plt.title('GH Filter')
+		plt.legend(loc=4)
+		plt.show()
+
+	def Algorithm(self):
+		print(f"""
+		Initialization
+
+		1. Initialize the state of the filter
+		2. Initialize our belief in the state
+
+		Predict
+
+		1. Use system behavior to predict state at the next time step
+		2. Adjust belief to account for the uncertainty in prediction
+
+		Update
+
+		1. Get a measurement and associated belief about its accuracy
+		2. Compute residual between estimated state and measurement
+		3. New estimate is somewhere on the residual line
+		""")
---
+++ gH Filter/tester.py
@@ -0,0 +1,61 @@
+from gh_filter import gH_filter
+import numpy as np
+import matplotlib.pyplot as plt
+
+def genData(x0, dx, count, noise_factor):
+	return [x0 + dx*i + np.random.randn()*noise_factor for i in range(count)]
+
+def accData(x0, dx, count, noise_factor,accel=0):
+	zs=[]
+	for i in range(count):
+		zs.append(x0 + dx*i + np.random.randn()*noise_factor)
+		dx+=accel
+	return zs
+
+
+
+def main():
+	
+	x0=160.0 #Init estimate
+	dx=1 #change rate
+	
+	g=6/10 
+	
+	"""
+		setting g~1, signal exactly follows the measurements implies NO NOISE REJECTION
+		setting g~0, signal ignore measurements and follows prediction, implies noise filterd but also LEGITIMATE CHANGE IN SIGNAL
+	
+	"""
+
+	h=2/3
+	
+	"""
+		setting h~1, signal reacts to change rate RAPIDLY, signal amplitude is less and settles on measurements fast
+		setting h~0, signal reacts to change less SLOWLY, signal amplitude is large and settles on measurements slowly
+	
+	"""
+
+	dt=1	#time step
+
+	#LESS NOISE
+	measurements=genData(x0,dx,count=30,noise_factor=1)
+	gh=gH_filter(measurements,x0,dx,g,h,dt,plot=True)
+	#gh.toString()
+	
+	#MORE NOISE
+	bad_measurements = genData(x0=5., dx=2., count=100, noise_factor=10)
+	gh_bad=gH_filter(bad_measurements,x0,dx,g,h,dt,plot=True)
+	#gh_bad.toString()
+
+	#EXTREME NOISE
+	extreme_measurements = genData(x0=5., dx=2., count=100, noise_factor=100)
+	gh_extreme=gH_filter(extreme_measurements,x0,dx,g,h,dt,plot=True)
+	#gh_extreme.toString()
+
+	#EFFECT OF ACCELERATION
+	accel_data = accData(x0=10., dx=0., count=20, noise_factor=0, accel=2.)
+	gh_accel=gH_filter(accel_data,x0,dx,g,h,dt,plot=True)
+
+	#gh.Algorithm()
+if __name__=="__main__":
+	main()
