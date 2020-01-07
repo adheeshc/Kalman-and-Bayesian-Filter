050f48f64903b477f025ce642e8048377045a557
Adheesh <adheeshc@github.com>
2020-01-06 20:59:04 -0500

    test done, bugs fixed in kf class

--- control_inputs.py
+++ control_inputs.py
@@ -1,7 +1,33 @@
 import numpy as np
-
 import sys
 sys.path.insert(2, './Multivariate Kalman Filter')
-
-from KF_Comparisons import *
+from kalman_filter_m import *
 from aux_classes import *
+
+def FirstOrderKF(R_in, Q_in, dt,measurements,B_in=[],u_in=[],P=100):
+    x=np.zeros(2)
+    P=np.array([P,1])
+    X=Gaussian(x,P)
+    F=np.zeros((2,2))
+    F[0][0]=dt
+    H=np.array([[1,0]])
+    B=B_in
+    u=u_in
+    Q=generateWhiteNoise(2,dt,Q_in)
+    R=R_in
+    kfm=Kalman_Filter_multi(X,F,H,B,Q,R, measurements,u)
+    return kfm
+    return kfm
+
+if __name__=="__main__":
+    R=3
+    dt=1
+    P=10
+    Q=0.1
+    B=np.array([[dt],[1]])
+    u=np.array([1])
+    #xs,zs=simulate_vel_system(Q,count=80)
+    zs = np.array([i + np.random.randn()*R for i in range(1, 100)])
+    kf2=FirstOrderKF(R,Q,dt,zs,B,u,P)
+    kf2.toPlot()
+    #kf2.plotResiduals(xs, 3)
